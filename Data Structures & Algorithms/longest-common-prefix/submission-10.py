776b6132c1a181e6549ce7ff9164ed66fe7f4895
varshacvenkat-web <varshacvenkat@gmail.com>
2026-09-25 15:57:25 -0400

    Add: longest-common-prefix - submission-10

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs[0])):
+            char=strs[0][i] #extract ith index from 0th variable in strs
+            for j in strs[1:]:
+                if i>=len(j) or char != j[i]:
+                    return strs[0][:i]
+        return strs[0]
+        
